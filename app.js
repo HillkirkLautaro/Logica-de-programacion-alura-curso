c1153d41045f5346ae60ce6f8c88ed0956d69827
Lautaro Hillkirk <107929009+HillkirkLautaro@users.noreply.github.com>
2025-07-23 15:56:53 -0300

    Update

---
+++ app.js
@@ -0,0 +1,25 @@
+// desafio 1
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+// desafio 2
+let name = "Lua"
+// desafio 3
+let age = 25
+// desafio 4
+let numerodeVentas = 50
+// desafio 5
+let saldoDisponible = 1000
+// desafio 6
+alert("¡Error! Completa todos los campos");
+// desafio 7
+let mensajedeError = "¡Error! Completa todos los campos";
+alert(mensajedeError);
+// desafio 8
+let nameUser = prompt("Ingrese su nombre:");
+// desafio 9
+let edad = prompt("Ingrese su edad:");
+// desafio 10 Le agregue un else JEJE
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+} else {
+    alert("Lo siento, necesitas ser mayor de edad para obtener tu licencia.");
+}
